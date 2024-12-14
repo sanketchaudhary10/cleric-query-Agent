2670c978b834ebc96e5cdc06c0c73df3866285ea
sanketchaudhary10 <67291486+sanketchaudhary10@users.noreply.github.com>
2024-12-14 14:46:30 -0600

    Updating code files

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pydantic import BaseModel
 
 class QueryRequest(BaseModel):
@@ -7,13 +6,3 @@ class QueryRequest(BaseModel):
 class QueryResponse(BaseModel):
     query: str
     answer: str
-=======
-from pydantic import BaseModel
-
-class QueryRequest(BaseModel):
-    query: str
-
-class QueryResponse(BaseModel):
-    query: str
-    answer: str
->>>>>>> c33188015ddd15f7c364ffa34c902c7ec91cd909
